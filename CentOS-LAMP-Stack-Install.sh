751082cb9c2b04797b26d48ad76d89300b352bd2
James Ambrose <galaxy2198@gmail.com>
2018-05-23 14:51:16 -0500

    added -y to yum commands to make it more user friendly

--- CentOS-LAMP-Stack-Install.sh
+++ CentOS-LAMP-Stack-Install.sh
@@ -1,10 +1,12 @@
-sudo yum install httpd
+sudo yum -y install httpd
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
-sudo yum install mariadb-server mariadb
+sudo yum -y install mariadb-server mariadb
 sudo systemctl start mariadb
 sudo mysql_secure_installation
 sudo systemctl enable mariadb.service
-sudo yum install php php-mysql php-fpm php-gd
+sudo yum -y install php php-mysql php-fpm php-gd
 sudo systemctl restart httpd.service
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+#sudo yum -y install php*
+
